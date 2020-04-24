ad1a598c21460afd39ac7e77a0aadc875000275c
voson wang <voson@ricnsmart.com>
2020-04-24 09:16:10 +0800

    refactor: LineNo add bson

--- rabbitmq.go
+++ rabbitmq.go
@@ -39,7 +39,7 @@ type (
 		Status   int       `bson:"Status"`
 		Host     string    `bson:"Host"`
 		CreateAt time.Time `bson:"CreateAt"`
-		LineNo   uint8     `json:"LineNo,string"`
+		LineNo   uint8     `bson:"LineNo" json:"LineNo,string"`
 	}
 
 	// 组件信息
