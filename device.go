bcfd031c193f83d025aca41669bfdc89b89f27ed
voson wang <voson@ricnsmart.com>
2020-04-24 11:14:58 +0800

    feat: DeviceStatusToString

--- device.go
+++ device.go
@@ -9,3 +9,19 @@ const (
 	ALERT   = 3 // 报警
 	Fault   = 4 // 故障
 )
+
+func DeviceStatusToString(status int) (result string) {
+	switch status {
+	case OFFLINE:
+		result = "离线"
+	case ONLINE:
+		result = "正常"
+	case WARN:
+		result = "预警"
+	case ALERT:
+		result = "报警"
+	case Fault:
+		result = "故障"
+	}
+	return
+}
