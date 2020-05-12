19773152419371920dd09e3ee4eda4ff1357dfbc
voson wang <voson@ricnsmart.com>
2020-05-12 14:01:54 +0800

    优化 rename

--- constant.go
+++ constant.go
@@ -4,15 +4,15 @@ const (
 	/*Pattern*/
 	OnlyDomainRecordPattern = `^[-a-zA-Z0-9.]+.[a-z]+$`
 
-	HasDomainRecordPattern = `[-a-zA-Z0-9.]+.[a-z]+`
+	ExistDomainRecordPattern = `[-a-zA-Z0-9.]+.[a-z]+`
 
-	HasNumPattern = `[0-9]+`
+	ExistNumPattern = `[0-9]+`
 
-	HasIpPattern = `(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d)`
+	ExistIpPattern = `(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d)`
 
 	OnlyIpPattern = `^(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d)$`
 
-	HasDomainRecordOrIpPattern = `[-a-zA-Z0-9.]+.[a-z]+|(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d)`
+	ExistDomainRecordOrIpPattern = `[-a-zA-Z0-9.]+.[a-z]+|(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d)`
 
 	OnlyIpOrDomainRecordPattern = `^([-a-zA-Z0-9.]+.[a-z]+|(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d))$`
 )
