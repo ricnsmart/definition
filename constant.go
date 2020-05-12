18753e0c3771c1ecf359078f549585f4b9060bdb
voson wang <voson@ricnsmart.com>
2020-05-12 12:13:59 +0800

    优化 rename

--- constant.go
+++ constant.go
@@ -1,18 +1,18 @@
 package definition
 
 const (
-	/*Regexp*/
-	OnlyDomainRecordRegexp = `^[-a-zA-Z0-9.]+.[a-z]+$`
+	/*Pattern*/
+	OnlyDomainRecordPattern = `^[-a-zA-Z0-9.]+.[a-z]+$`
 
-	HasDomainRecordRegexp = `[-a-zA-Z0-9.]+.[a-z]+`
+	HasDomainRecordPattern = `[-a-zA-Z0-9.]+.[a-z]+`
 
-	HasNumRegexp = `[0-9]+`
+	HasNumPattern = `[0-9]+`
 
-	HasIp = `(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d)`
+	HasIpPattern = `(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d)`
 
-	OnlyIP = `^(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d)$`
+	OnlyIpPattern = `^(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d)$`
 
-	HasDomainRecordOrIP = `[-a-zA-Z0-9.]+.[a-z]+|(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d)`
+	HasDomainRecordOrIpPattern = `[-a-zA-Z0-9.]+.[a-z]+|(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d)`
 
-	OnlyIPOrDomainRecord = `^([-a-zA-Z0-9.]+.[a-z]+|(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d))$`
+	OnlyIpOrDomainRecordPattern = `^([-a-zA-Z0-9.]+.[a-z]+|(1\d\d|2[0-5][0-5]|[1-9]\d|[1-9]).((1\d\d|2[0-5][0-5]|[1-9]\d|\d).){2}(1\d\d|2[0-5][0-5]|[1-9]\d|\d))$`
 )
