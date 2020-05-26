5d40095f9f79431106d2ba538bfb7156603b0a59
voson wang <voson@ricnsmart.com>
2020-05-26 12:09:06 +0800

    add device type

--- device.go
+++ device.go
@@ -2,6 +2,17 @@ package definition
 
 const (
 
+	// 设备类型
+	RCNVJ   = "RCNVJ"
+	RCN350F = "RCN350F"
+	PMC350  = "PMC350"
+	PMC350F = "PMC350F"
+	ASCP200 = "ASCP200"
+	GS524N  = "GS524N"
+	GS894C  = "GS894C"
+	TK82    = "TK82"
+	TK83    = "TK83"
+
 	/*设备/线路状态*/
 	OFFLINE = 0 // 离线
 	ONLINE  = 1 // 在线
