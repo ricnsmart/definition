c3b83ac87bb4c8c235fd565330e9581327a371db
voson@ricnsmart.com <voson@2018>
2020-06-02 20:48:44 +0800

    json序列化、bson存储统一改为小写

--- rabbitmq.go
+++ rabbitmq.go
@@ -30,68 +30,68 @@ const (
 type (
 	// 向设备服务请求的格式
 	MetricRequest struct {
-		SN      string
-		Method  uint8 `json:"method,string"`
-		Metrics string
-		Value   string
-		LineNo  uint8 `json:"line_no,string"`
+		SN      string `json:"sn" bson:"sn"`
+		Method  uint8  `json:"method,string" bson:"method"`
+		Metrics string `json:"metrics" bson:"metrics"`
+		Value   string `json:"value" bson:"value"`
+		LineNo  uint8  `json:"line_no,string"`
 	}
 
 	// 设备服务对请求的响应
 	MetricResponse struct {
-		Code    string
-		Message string
-		Data    string
+		Code    string `json:"code" bson:"code"`
+		Message string `json:"message" bson:"message"`
+		Data    string `json:"data" bson:"data"`
 	}
 
 	// 设备状态
 	DeviceStatus struct {
-		ChangeTime time.Time `bson:"change_time"` // 状态变更时间
-		SN         string    `bson:"sn"`
-		DeviceType string    `bson:"device_type"`
-		Status     int       `bson:"status" `
-		Host       string    `bson:"host"`
+		SN         string    `json:"sn" bson:"sn"`
+		DeviceType string    `json:"device_type" bson:"device_type"`
+		Status     int       `json:"status" bson:"status" `
+		Host       string    `json:"host" bson:"host"`
+		ChangeTime time.Time `json:"change_time" bson:"change_time"` // 状态变更时间
 	}
 
 	// 线路状态
 	LineStatus struct {
-		ChangeTime time.Time `bson:"change_time"`
-		SN         string    `bson:"sn"`
-		Status     int       `bson:"status"`
+		SN         string    `json:"sn" bson:"sn"`
+		Status     int       `json:"status" bson:"status"`
 		LineNo     int       `json:"line_no" bson:"line_no"`
+		ChangeTime time.Time `json:"change_time" bson:"change_time"`
 	}
 
 	// 服务信息
 	// 为了防止port设置失误，导致端口冲突等问题，还是显式申明port
 	Component struct {
-		StartupTime time.Time `bson:"startup_time"` // 设备服务启动时间
-		Name        string    `bson:"name"`
-		Port        int       `bson:"port"`
-		Host        string    `bson:"host"`
-		Version     string    `bson:"version"`
-		BuildTime   string    `bson:"build_time"` // 设备服务构建时间
+		Name        string    `json:"name" bson:"name"`
+		Port        int       `json:"port" bson:"port"`
+		Host        string    `json:"host" bson:"host"`
+		Version     string    `json:"version" bson:"version"`
+		BuildTime   string    `json:"build_time" bson:"build_time"`   // 设备服务构建时间
+		StartupTime time.Time `json:"start_time" bson:"startup_time"` // 设备服务启动时间
 	}
 
 	// 设备警报详细信息
 	DeviceAlarm struct {
-		SendTime   time.Time `bson:"send_time"` // 警报发出时间
-		SN         string    `bson:"sn"`
-		Metric     string    `bson:"metric"`
-		AlarmType  int       `bson:"alarm_type"`
-		DeviceType string    `bson:"device_type"`
-		Current    float64   `bson:"current"`
-		SetValue   float64   `bson:"set_value"`
+		SN         string    `json:"sn" bson:"sn"`
+		Metric     string    `json:"metric" bson:"metric"`
+		AlarmType  int       `json:"alarm_type" bson:"alarm_type"`
+		DeviceType string    `json:"device_type" bson:"device_type"`
+		Current    float64   `json:"current" bson:"current"`
+		SetValue   float64   `json:"set_value" bson:"set_value"`
+		SendTime   time.Time `json:"send_time" bson:"send_time"` // 警报发出时间
 	}
 
 	// 线路警报详细信息
 	LineAlarm struct {
-		SendTime   time.Time `bson:"send_time"` // 警报发出时间
-		SN         string    `bson:"sn"`
+		SN         string    `json:"sn" bson:"sn"`
 		LineNo     int       `json:"line_no" bson:"line_no"`
-		Metric     string    `bson:"metric"`
-		AlarmType  int       `bson:"alarm_type"`
-		DeviceType string    `bson:"device_type"`
-		Current    float64   `bson:"current"`
-		SetValue   float64   `bson:"set_value"`
+		Metric     string    `json:"metric" bson:"metric"`
+		AlarmType  int       `json:"alarm_type" bson:"alarm_type"`
+		DeviceType string    `json:"device_type" bson:"device_type"`
+		Current    float64   `json:"current" bson:"current"`
+		SetValue   float64   `json:"set_value" bson:"set_value"`
+		SendTime   time.Time `json:"send_time" bson:"send_time"` // 警报发出时间
 	}
 )
